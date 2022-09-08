d5884261c824139612702ebdf15c7a7cca95cebe
Jonas Birmé <jonas.birme@eyevinn.se>
2022-09-08 11:10:05 +0200

    feat: added an nginx proxy in front of the management API (#2)
    * feat: added an nginx proxy in front of the management API
    
    * fix: use the application's health endpoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@
 cat > config.json << EOF
 {
   "logStdOut": ${LOG_STD_OUT},
-  "port": ${HTTP_PORT},
+  "port": 8181,
   "logLevel": "${LOG_LEVEL}",
   "ice.singlePort": ${UDP_PORT},
   "ice.udpPortRangeLow": ${UDP_PORT_LOW},
@@ -26,4 +26,22 @@ cat > config.json << EOF
 }
 EOF
 
+cat > nginx.conf << EOF
+events {}
+http {
+  server {
+    listen  0.0.0.0:${HTTP_PORT};
+    location / {
+      proxy_set_header Host \$host;
+      proxy_pass http://127.0.0.1:8181;
+    }
+    location = / {
+      access_log off;
+      proxy_pass http://127.0.0.1:8181/about/health;
+    }
+  }
+}
+EOF
+
+/usr/sbin/nginx -c /app/nginx.conf -g "daemon on;"
 ./smb config.json
