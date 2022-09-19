5c98bfc3b92e1acebe0d65f45a6f5f28d2bef009
Jonas Birmé <jonas.birme@eyevinn.se>
2022-09-19 20:38:06 +0200

    fix: lock to a specific SMB commit

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 git clone https://github.com/marcusspangenberg/SymphonyMediaBridge.git
 pushd SymphonyMediaBridge
+git checkout 953ae35f6c3b276a40bcc45dbe4fb4b961c9bfec
 CC=clang CXX=clang++ cmake -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" .
 CC=clang CXX=clang++ make -j5
 popd
