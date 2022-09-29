58cdce387e4e9573eb1791701fb2b58db21cffd8
Marcus Spangenberg <marcus.spangenberg@eyevinn.se>
2022-09-29 08:39:51 +0200

    chore: update sfu commit

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/marcusspangenberg/SymphonyMediaBridge.git
 pushd SymphonyMediaBridge
-git checkout 953ae35f6c3b276a40bcc45dbe4fb4b961c9bfec
+git checkout marcus/fix_broken_outbound_cleanup
 CC=clang CXX=clang++ cmake -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" .
 CC=clang CXX=clang++ make -j5
 popd
