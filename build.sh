cd4d260470549e1ed24d33a09ff118da31da84e8
Marcus Spangenberg <marcus.spangenberg@eyevinn.se>
2023-02-10 09:23:58 +0100

    Uplift smb to 2.2.0-413

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-git clone https://github.com/marcusspangenberg/SymphonyMediaBridge.git
+SMB_VERSION=2.2.0-413
+
+git clone https://github.com/eyevinn/SymphonyMediaBridge.git
 pushd SymphonyMediaBridge
-git checkout marcus/fix_broken_outbound_cleanup
+git ${SMB_VERSION}
 CC=clang CXX=clang++ cmake -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" .
 CC=clang CXX=clang++ make -j5
 popd
